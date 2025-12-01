f2dd84a99ec3590b4ae324eba2f8a19087bc7d26
Andrii <andioblack24@gmail.com>
2025-12-01 14:33:01 +0200

    Solution: Task 1.2 variables and data type

---
+++ Tasks/jbkf/T1P2/t1p2.java
@@ -0,0 +1,19 @@
+public class t1p2 {
+  public static void main(String[] args) {
+    int x;
+    x = -5;
+
+    // int a = 0.0, b = 7; Value int can not be 0.0
+
+    int b = 3;
+    double c;
+    c = 5.0;
+
+    // int a, b, c = 5;
+    // b = 5c; Incorrect b 
+    // a = -b;
+
+    // double a = 3.6, b; 
+    // int b = 4; Second time b called
+  }
+}
