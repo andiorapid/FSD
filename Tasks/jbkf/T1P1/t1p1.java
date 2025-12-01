df812f751c3b05f30a99b78f8149bc9edb5e8e06
Andrii <andioblack24@gmail.com>
2025-12-01 11:21:42 +0200

    Feat: Task 1.1 variables and data types

---
+++ Tasks/jbkf/T1P1/t1p1.java
@@ -0,0 +1,25 @@
+// 1.1
+
+public class t1p1 {
+  public static void main(String[] args) {
+    // int 1x; Name starts with 1
+
+    // double t;
+    // int t; Second time t called
+
+    // int f, f; Second time f called
+
+    // int x, X;
+    // double a; a1; Used ; instead of ,
+
+    // String kniga1; kniga2; Used ; instead of ,
+
+    char znak1, znak2, znak_1;
+
+    // int x, a, b;
+    // double y, z, x; Second time x called
+
+    int x;
+    double X;
+  }
+}
