cec029a03adeedc8fb9577b346e42dd457130893
Andrii <andioblack24@gmail.com>
2025-12-05 22:03:24 +0200

    Solution: Task 2.5 input output
    2.5.1 If number is 6 aa = 36, a * 10 = 60
    2.5.2 If number is -6 aa = 36, a * 10 = -60
    2.5.2 Changes in program, to make it more user friendly

--- Tasks/jbkf/T2P5/t2p5.java
+++ Tasks/jbkf/T2P5/t2p5.java
@@ -3,8 +3,9 @@ import java.util.*;
 class t2p5 {
   static Scanner reader = new Scanner(System.in);
   public static void main(String[] args) {
+    System.out.print("Write your number: ");
     int a = reader.nextInt();
-    System.out.println("aa = " + (a * a));
-    System.out.println("a10 = " + (a * 10));
+    System.out.println("Result aa = " + (a * a));
+    System.out.println("Result a10 = " + (a * 10));
   }
 }
