2ea0240a7319c0ec797b44b60e104172bb9ace25
Andrii <andioblack24@gmail.com>
2025-12-05 21:59:48 +0200

    Solution: Task 2.5 input outpur

---
+++ Tasks/jbkf/T2P5/t2p5.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class t2p5 {
+  static Scanner reader = new Scanner(System.in);
+  public static void main(String[] args) {
+    int a = reader.nextInt();
+    System.out.println("aa = " + (a * a));
+    System.out.println("a10 = " + (a * 10));
+  }
+}
