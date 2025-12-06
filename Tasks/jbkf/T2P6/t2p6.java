aeb699c3565340caf1dc4350ab4edf261b46a68b
Andrii <andioblack24@gmail.com>
2025-12-06 23:53:57 +0200

    Solution: Task 2.6 input output
    2.5.1 If number is 6 output = 6 7 8
    2.5.2 If number is 0 output = 0 1 2

---
+++ Tasks/jbkf/T2P6/t2p6.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class t2p6 {
+  static Scanner reader = new Scanner(System.in);
+  public static void main(String[] args) {
+    int a = reader.nextInt();
+    System.out.print(" " + (a));
+    System.out.print(" " + (a + 1));
+    System.out.print(" " + (a + 2));
+  }
+}
