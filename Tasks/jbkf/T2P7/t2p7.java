52e87e44d3622a689884be877a398aa1fbfa2e14
Andrii <andioblack24@gmail.com>
2025-12-07 14:02:22 +0200

    Solution: 2.7 input output
    2.7.1 If number is 100, result of program would be -900
    2.7.2 If number is -1, result of program would be 9

---
+++ Tasks/jbkf/T2P7/t2p7.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class t2p7 {
+  static Scanner reader = new Scanner(System.in);
+  public static void main(String[] args) {
+    int a = reader.nextInt();
+    System.out.println(a - 10 * a);
+  }
+}
