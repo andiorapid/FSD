652e4c004edcae2a33ef4ad453eb9848d627d0ed
Andrii <andioblack24@gmail.com>
2025-12-07 13:49:59 +0200

    Solution: Task 2.6 input output
    2.6.3 Changes in program, to make it more user friendly

--- Tasks/jbkf/T2P6/t2p6.java
+++ Tasks/jbkf/T2P6/t2p6.java
@@ -3,9 +3,10 @@ import java.util.*;
 public class t2p6 {
   static Scanner reader = new Scanner(System.in);
   public static void main(String[] args) {
+    System.out.print("Write your number: ");
     int a = reader.nextInt();
-    System.out.print(" " + (a));
-    System.out.print(" " + (a + 1));
-    System.out.print(" " + (a + 2));
+    System.out.print(" Your number a = " + (a));
+    System.out.print(" Result a + 1 = " + (a + 1));
+    System.out.print(" Result a + 2 = " + (a + 2));
   }
 }
