a0925562c40a0356154e35b69e18e36fc0eb9118
Andrii <andioblack24@gmail.com>
2025-12-08 10:52:15 +0200

    Solution: Task 2.7 input output

--- Tasks/jbkf/T2P7/t2p7.java
+++ Tasks/jbkf/T2P7/t2p7.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class t2p7 {
   static Scanner reader = new Scanner(System.in);
   public static void main(String[] args) {
+    System.out.print("Write your number: ");
     int a = reader.nextInt();
     System.out.println(a - 10 * a);
   }
