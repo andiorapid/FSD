0be746b2f09463679461e73b485ee701747b4459
Andrii <andioblack24@gmail.com>
2025-12-09 22:29:01 +0200

    Solution: Task 2.11 input output

---
+++ Tasks/jbkf/T2P11/t2p11.java
@@ -0,0 +1,8 @@
+public class t2p11 {
+  public static void main(String[] args) {
+    System.out.println("*****************");
+    System.out.println("* Bert Lewangoa *");
+    System.out.println("* 16-55-443-842 *");
+    System.out.println("*****************");
+  }
+}
