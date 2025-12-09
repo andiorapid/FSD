431a55c249a1bbd4456c204d6745bf480f97fb65
Andrii <andioblack24@gmail.com>
2025-12-09 22:16:09 +0200

    Solution: Task 2.10 input output

---
+++ Tasks/jbkf/T2P10/t2p10.java
@@ -0,0 +1,9 @@
+public class t2p10 {
+  public static void main(String[] args) {
+    int a = 6;
+    int b = 9;
+    System.out.printf("Number a is %d. Number b is %d\n", a, b);
+    System.out.printf("Multiplication of numbers is %d\n", a * b);
+    System.out.printf("Sum of numbers is %d\n", a + b);
+  }
+}
