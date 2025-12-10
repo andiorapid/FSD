d3ce68fc5ba00ba5b7e58468b3fc61b77e4835bb
Andrii <andioblack24@gmail.com>
2025-12-10 22:19:38 +0200

    Solution: Task 2.14 input output

---
+++ Tasks/jbkf/T2/T2P14/t2p14.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class t2p14 {
+  static Scanner reader = new Scanner(System.in);
+  public static void main(String[] args) {
+    System.out.print("Write your number: ");
+    int a = reader.nextInt();
+    System.out.printf("Result = %d\n", a * 10);
+  }
+}
