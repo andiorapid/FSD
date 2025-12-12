b290c0a4e267ff355714d66bead8ad2f64994063
Andrii <andioblack24@gmail.com>
2025-12-12 22:22:23 +0200

    Solution: Task 2.16 input output

---
+++ Tasks/jbkf/T2/T2P16/t2p16.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class t2p16 {
+  static Scanner reader = new Scanner(System.in);
+  public static void main(String[] args) {
+    System.out.print("Write your nubmer: ");
+    int a = reader.nextInt();
+    System.out.printf("Your number + 6 = %d\n", a + 6);
+    System.out.printf("Your number - 12 = %d\n", a - 12);
+  }
+}
